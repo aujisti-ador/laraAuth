4ae65d64a463f642d663c5377e27c737a002e28b
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-03-11 18:12:19 +0600

    smtp added for reset pw in mailtrap.io

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('send_test_email', function(){
+    Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+    {
+        $message->subject('Mailgun and Laravel are awesome!');
+        $message->from('postmaster@sandbox1e49cff6471d42f4a3e51b120b655eb1.mailgun.org', 'Website Name');
+        $message->to('aujisti.ador@gmail.com');
+    });
+});
